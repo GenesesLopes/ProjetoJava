6e276da1bf7a0bd6aaeeb6215a0ae74e1818ed05
genesesLopes <genesesslopes@gmail.com>
2018-05-29 11:40:36 -0300

    tentativa de thread na aplicação

--- Crud2.1/src/socket/Cliente.java
+++ Crud2.1/src/socket/Cliente.java
@@ -1,17 +1,22 @@
 package socket;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 
-
 public class Cliente {
-	Socket cliente;
+	/*Socket cliente;
 	private int requisicao;
 	InputStream conexao;
 	ObjectOutputStream saida;
@@ -24,10 +29,13 @@ public class Cliente {
 		
 		if(requisicao == 1) {
 			listarArquivosCliente();
-		}			
+		}else if(requisicao == 2) {
+			baixarArquivosCliente();
+		}
 		
 	}
 	
+
 	//metodos gets e sets
 	public String[] getArquivos() {
 		return arquivos;
@@ -35,6 +43,32 @@ public class Cliente {
 
 	public void setArquivos(String[] arquivos) {
 		this.arquivos = arquivos;
+	}
+	
+	public void baixarArquivosCliente() {
+		try {
+			String teste = null;
+			cliente = new Socket("localhost",2474);
+			OutputStream out = cliente.getOutputStream();
+			InputStream in = cliente.getInputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(out);
+			InputStreamReader isw = new InputStreamReader(in);
+			BufferedWriter writer = new BufferedWriter(osw);
+			BufferedReader reader = new BufferedReader(isw);
+			writer.write("Enviando esse texto do cliente");
+			writer.flush();
+			if(reader.ready())
+				teste = reader.readLine();
+			System.out.println(teste);
+			reader.close();
+			writer.close();
+			cliente.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	//método para listar arquivos para o cliente
@@ -50,7 +84,8 @@ public class Cliente {
 			setArquivos(objeto.readUTF().split(","));
 			/*for (int i = 0; i < getArquivos().length; i++) {
 				System.out.println(getArquivos()[i]);
-			}*/
+				antigo ate aqui
+			}
 			
 			//fechando conexões
 			objeto.close();
@@ -63,10 +98,47 @@ public class Cliente {
 		}
 		
 		
-	}
-	/*
-	public static void main(String[] args) {
-		new Cliente(1);
 	}*/
+	
+	public static void main(String[] args) {
+		try {
+			final Socket cliente = new Socket("localhost",2473);
+			//lendo mensagens do servidor
+			new Thread() {
+				@Override
+				public void run() {
+					try {
+						BufferedReader leitor = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+						
+						
+						while(true) {
+							String mensagem = leitor.readLine();
+							System.out.println("O servidor disse: "+mensagem);
+						}
+						
+					}  
+					catch (IOException e) {
+						System.out.println("impossivel ler a mensagem do servidor");
+						e.printStackTrace();
+					}
+				}
+			}.start();
+			//escrevendo para o servidor
+			PrintWriter escreveServidor = new PrintWriter(cliente.getOutputStream());
+			BufferedReader leitorTerminal = new BufferedReader(new InputStreamReader(System.in));
+			while(true) {
+				String mensagemTerminal = leitorTerminal.readLine();
+				escreveServidor.println(mensagemTerminal);
+			}
+			
+		} catch (UnknownHostException e) {
+			System.out.println("Endereço inválido");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Servidor Fora do ar");
+			e.printStackTrace();
+		}
+		
+	}
 
 }
--- Crud2.1/src/socket/Servidor.java
+++ Crud2.1/src/socket/Servidor.java
@@ -1,23 +1,59 @@
 package socket;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Servidor {
-	Socket sv;
+	
+	/*Socket sv;
 	ServerSocket servidor;
 	String files = null;
 	ObjectInputStream entrada;
+	boolean flag = true;
 
 	//construtor
 	public Servidor() {
 		listaArquivosServidor();
+		baixaArquivosServidor();
+	}
+	
+	public void baixaArquivosServidor() {
+		try {
+			servidor = new ServerSocket(2474);
+			sv = servidor.accept();
+			System.out.println("Conexão estabelecida no Servidor pela porta 2474...");
+			InputStream in = sv.getInputStream();
+			OutputStream out = sv.getOutputStream();
+			InputStreamReader isr = new InputStreamReader(in);
+			OutputStreamWriter iwr = new OutputStreamWriter(out);
+			BufferedReader reader = new BufferedReader(isr);
+			BufferedWriter write = new BufferedWriter(iwr);
+			while(reader.ready()) {
+				String resposta = reader.readLine();
+				System.out.println(resposta);
+				write.write("Mensagem do servidor");
+			}
+			
+			reader.close();
+			write.close();
+			sv.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
@@ -60,10 +96,30 @@ public class Servidor {
 			e.printStackTrace();
 		}
 		
-	}
+	}*/
+	
+	
 	
 	public static void main(String[] args) {
-		new Servidor();
+		ServerSocket servidor = null;
+		try {
+			System.out.println("Iniciando o servidor");
+			servidor =  new ServerSocket(2473);
+			System.out.println("Servidor iniciado");
+			while(true) {
+				Socket cliente = servidor.accept();
+				new GerenciadorDeClientes(cliente);
+			}
+		} catch (IOException e) {
+			try {
+				if(servidor != null)
+					 servidor.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 
 }
